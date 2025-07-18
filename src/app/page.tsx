ef58c5c24ab30c1d3f2fd5821fa4515755077fe7
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2025-07-18 23:03:16 +0600

    02: Customization for Button,textarea etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Progress } from "@/components/ui/progress";
+import { Checkbox } from "@/components/ui/checkbox";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">hello world</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>Hello World</Button>
+        </div>
+        <div>
+          <Input placeholder="Input..." />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea value={"text area"} />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
